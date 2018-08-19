547d49bb2abd3dacb3c6c9eaa659ca5fcf209e41
Sven Meyer <git_dev@svenmeyer.com>
2018-08-20 01:10:38 +1000

    no fixed contract parameter at deployment time

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,5 +1,5 @@
 var ERC721Vehicle = artifacts.require("./ERC721Vehicle.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC721Vehicle, "Porsche", "POR");
+  deployer.deploy(ERC721Vehicle);
 };
