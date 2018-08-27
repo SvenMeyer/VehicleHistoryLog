67e2f034b43e0cce1e433b82c7581fee5f64486d
Sven Meyer <git_dev@svenmeyer.com>
2018-08-27 22:37:46 +1000

    now works with testContract.owner = contract.deployer

--- test/TestERC721Vehicle.sol
+++ test/TestERC721Vehicle.sol
@@ -7,7 +7,31 @@ import "../contracts/ERC721Vehicle.sol";
 contract TestERC721Vehicle {
     //define a contract-wide variable containing the smart contract to be tested, 
     //calling the DeployedAddresses smart contract to get its address.
-    ERC721Vehicle vc = ERC721Vehicle(DeployedAddresses.ERC721Vehicle());
+    // ERC721Vehicle vc = ERC721Vehicle(DeployedAddresses.ERC721Vehicle());
+
+    ERC721Vehicle vc;
+
+    // uint public initialBalance = 10 ether;
+
+    // function () public payable {}
+
+    // we need this 'beforeEach()' function to make this test work
+    // with owner of this test-contract = deployer of contract
+    // https://michalzalecki.com/ethereum-test-driven-introduction-to-solidity
+    // https://github.com/MichalZalecki/tdd-solidity-intro/blob/master/test/FundingTest.sol
+
+    function beforeEach() public {
+        vc = new ERC721Vehicle();
+    }
+
+    function testSettingAnOwnerDuringCreation() public {
+        Assert.equal(vc.creator(), this, "An creator is different than a deployer");
+    }
+
+    function testSettingAnOwnerOfDeployedContract() public {
+        vc = ERC721Vehicle(DeployedAddresses.ERC721Vehicle());
+        Assert.equal(vc.creator(), msg.sender, "An creator is different than a deployer");
+    }
     
 	// Test to mint 2 new token = 2 new vehicles
 
@@ -23,24 +47,24 @@ contract TestERC721Vehicle {
 		bytes32 ein_2   = 'BFD';
         uint tokenId_2 = vc.mintNewVehicleToken(model_2, vin_2, ein_2);
         Assert.equal(tokenId_2, 2, "tokenId of second vehicle should be 2");
-    }
+    // }
 
     // tokenId = serial is autoincremented
     // Test if the value can be retrieved and if the value is 2
 
-    function testGetLastSerial() public {
+    // function testGetLastSerial() public {
         Assert.equal(vc.getLastSerial(), 2, "last serial should be 2");
-    }
+    // }
 
     // With 2 token/vehicles being added by now, tokenId 0 should be not valid
     // tokenId 1 and 2 should represent the 2 token/vehicles created before
     // there should not be a volid tokenId/vehicle 3
 
-    function testIsValidToken() public {
+    // function testIsValidToken() public {
         Assert.equal(vc.isValidToken(0), false, "isValidToken(0) should be false");
         Assert.equal(vc.isValidToken(1), true , "isValidToken(1) should be true");
         Assert.equal(vc.isValidToken(2), true , "isValidToken(2) should be true");
         Assert.equal(vc.isValidToken(3), false, "isValidToken(3) should be false");
+    // }
     }
-
 }
