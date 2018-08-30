4b31d0e54222289fd8fb9bcf71103a8867b7873b
Sven Meyer <git_dev@svenmeyer.com>
2018-08-30 23:32:27 +1000

    if MetaMask is on Rinkeby network then deployed contract will be used

--- client/lib/getContract.js
+++ client/lib/getContract.js
@@ -1,7 +1,18 @@
 const getContractInstance = async (web3, contractDefinition) => {
   // get network ID and the deployed address
   const networkId = await web3.eth.net.getId()
-  const deployedAddress = contractDefinition.networks[networkId].address
+  console.log( {networkId} );
+
+  var deployedAddress;
+
+  if (networkId == 4) {
+    console.log('Will use the deployed contract on Rinkeby Network');
+    deployedAddress = '0xc722344f2fba0effb97550f075481041b6e00909';
+  }
+  else {
+    deployedAddress = contractDefinition.networks[networkId].address;
+  }
+  console.log('deployedAddress =', deployedAddress);
 
   // create the instance
   const instance = new web3.eth.Contract(
