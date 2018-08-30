371b382fd1eaf2caf12d7368a7bfdbfc4eb95980
Sven Meyer <git_dev@svenmeyer.com>
2018-08-30 21:25:04 +1000

    check if truffle compile produces correct json

---
+++ check_truffle_json.sh
@@ -0,0 +1,4 @@
+grep -c "appendLogEntry" contracts/ERC721Vehicle.sol
+grep -c "appendLogEntry" build/contracts/ERC721Vehicle.json
+grep -c "getLastSerial" contracts/ERC721Vehicle.sol
+grep -c "getLastSerial" build/contracts/ERC721Vehicle.json
