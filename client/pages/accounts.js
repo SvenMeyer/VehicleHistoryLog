3736621515f8a34cfb6e5add7b162600e6ddcccb
Sven Meyer <git_dev@svenmeyer.com>
2018-09-05 22:34:56 +1000

    Menu and nicer layout for accounts page

--- client/pages/accounts.js
+++ client/pages/accounts.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
 import Web3Container from '../lib/Web3Container'
+import Layout from '../components/Layout';
 
 const Accounts = ({ accounts }) => (
   <div>
-    <h1>My Accounts</h1>
+    <Layout />
+    <h3>Accounts</h3>
     <pre>{JSON.stringify(accounts, null, 4)}</pre>
     <div><Link href='/dapp'><a>My Dapp</a></Link></div>
     <div><Link href='/'><a>Home</a></Link></div>
