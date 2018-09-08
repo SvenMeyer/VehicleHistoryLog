49a04ecf9f4e0fb9847c3d0a15bc8a9d9339defc
Sven Meyer <git_dev@svenmeyer.com>
2018-09-08 23:29:26 +1000

    pages/vehicles/list

--- client/pages/vehicles/list.js
+++ client/pages/vehicles/list.js
@@ -1,101 +1,138 @@
 import React from 'react'
+import { Router }    from '../../routes';
 import Web3Container from '../../lib/Web3Container';
-import { Form, Button, Input, Message, Table } from 'semantic-ui-react';
-import Layout from '../../components/Layout';
-import { Router } from '../../routes';
-import RequestRow from '../../components/RequestRow';
+import Layout        from '../../components/Layout';
 
+// http://react.semantic-ui.com/collections/table
+import { Table } from 'semantic-ui-react';
 
 class ERC721VehicleList extends React.Component {
-	state = {
-		model: '',
-		vin: '',
-		ein: '',
-		errorMessage: '',
-		loading: false,
-		lastSerial: 0
-	};
+	
+	constructor(props) {
+		super(props);
 
-	getLastSerial = async () => {
-		console.log('getLastSerial -------------------------')
-		console.log(this.state.lastSerial)
-		const { accounts, contract } = this.props
-		const response = await contract.methods.getLastSerial().call({ from: accounts[0] })
-		console.log('getLastSerial : response =', response)
-		this.setState({ lastSerial: Number(response) })
-		console.log({ response })
-		console.log('lastSerial =', this.state.lastSerial)
-	};
+		this.state = {
+			model: '',
+			vin: '',
+			ein: '',
+			errorMessage: '',
+			loading: false,
+			lastSerialInt: 0,
+			vehicles: [],
+			dataLoaded: false
+		};
 
-	/* on button submit : create a new ERC721Vehicle from Form Data */
+		this.props.contract.methods.getLastSerial().call({ from: this.props.accounts[0] })
+			.then(response => {
+				console.log('ERC721VehicleList.constructor : getLastSerial.response =', Number(response));
+				this.setState({lastSerialInt: Number(response)})
+			});
 
-	onSubmit = async event => {
-		console.log('create button clicked');
-		event.preventDefault();
+	}
 
-		this.setState({ loading: true, errorMessage: '' });
-
-		try {
-			// const accounts = await this.state.web3.eth.getAccounts();
-			// const { accounts, contract } = this.props
-			const { accounts, contract } = this.props
-			console.log('contract :', contract);
-			console.log('accounts :', accounts);
-			const {model, vin, ein} = this.state;
-			console.log('model, vin, ein =', model, vin, ein);
-			const tokenId = await contract.methods.mintNewVehicleToken(model, vin, ein).send({ from: accounts[0] })
-			console.log('created tokenId :', tokenId)
-			Router.pushRoute('/');
-		} catch (err) {
-			this.setState({ errorMessage: err.message });
+	async componentDidUpdate() {
+		if (this.state.lastSerialInt > 0 && this.state.vehicles.length == 0) {
+			var i = this.state.lastSerialInt;
+			// var v = [];
+			while (i > 0) {
+				this.state.vehicles[i] = await this.props.contract.methods.getVehicleData(i).call({ from: this.props.accounts[0] });
+				console.log('this.state.vehicles[', i, ']=', this.state.vehicles[i]);
+				i--;
+			}
 		}
+		if (this.state.vehicles.length > this.state.lastSerialInt && this.state.dataLoaded == false) {
+			// this.setState({ vehicles: v }); // Warning: Can't call setState (or forceUpdate) on an unmounted component
+			this.setState({dataLoaded: true});
+		}
+	}
 
-		this.setState({ loading: false });
-	};
+	renderRows() {
+		var rows = [];
+		var i = this.state.vehicles.length - 1;
+		console.log('renderRows : i=', i);
+		while (i > 0) {
+			rows.push(this.renderRow(i));
+			i--
+		}
+		return(rows);
+	}
 
-	/* render page */
+	renderRows_map() {
+		return this.props.vehicles.map((vehicle, index) => {
+			if (index != 0) return (
+				renderRow(index)
+			);
+		});
+	}
+
+	renderRow(i) {
+		const { Row, Cell } = Table;
+		// const v = this.state.vehicles[i];
+		console.log('renderRow: i =', i);
+		console.log(this.state.vehicles[i].model);
+		console.log(this.state.vehicles[i].vin);
+		console.log(this.state.vehicles[i].ein);
+		// console.log('v =', v);
+		return (
+			<Row>
+				<Cell>{i}</Cell>
+				<Cell>{this.state.vehicles[i].model}</Cell>
+				<Cell>{this.state.vehicles[i].vin}</Cell>
+				<Cell>{this.state.vehicles[i].ein}</Cell>
+				<Cell>(owner)</Cell>
+				<Cell>(service)</Cell>
+				<Cell>(DocURI)</Cell>
+			</Row>
+		);
+	}
+
+	/* render page ****************************************************** */
 
 	render() {
 		const { Header, HeaderCell, Row, Cell, Body } = Table;
 
-		const { accounts, contract } = this.props;
+		const { web3, accounts, contract } = this.props;
+		// const _lastSerial = await contract.methods.getLastSerial().call({ from: accounts[0] })
+		// console.log('ERC721VehicleList.constructor : _lastSerial =', _lastSerial);
+		/*
+		this.props.contract.methods.getLastSerial().call({ from: this.props.accounts[0] })
+			.then( response => this.setState( {lastSerial: Number(response)} ) );
+		*/
+		// const lastVehicle = await contract.methods.getVehicleData(1).call();
+		/*
+		console.log('lastVehicle =', lastVehicle);
+
+		console.log('this.props.requests   =', this.props.requests);
+		console.log('this.props.lastSerial =', this.props.lastSerial);
+		console.log('this.props =', this.props);
+		*/
 
 		return (
 			<Layout>
 				<h3>ERC721Vehicle List</h3>
+				<p />
+				<pre>Account    : {accounts[0]}</pre>
+				<pre>lastSerial : {this.state.lastSerialInt}</pre>
+				<pre>dataLoaded : {this.state.dataLoaded ? 'true' : 'false'}</pre>
+				
+				<Table>
 
-				{/* lastSerial : {await getLastSerial()} */}
+					<Header>
+						<Row>
+							<HeaderCell>Id</HeaderCell>
+							<HeaderCell>model</HeaderCell>
+							<HeaderCell>vin</HeaderCell>
+							<HeaderCell>ein</HeaderCell>
+							<HeaderCell>owner</HeaderCell>
+							<HeaderCell>service</HeaderCell>
+							<HeaderCell>doc URI</HeaderCell>
+						</Row>
+					</Header>
 
-				<pre>Account: {accounts[0]}</pre>
-
-					<Table>
-
-						<Header>
-								<Row>
-										<HeaderCell>Id</HeaderCell>
-										<HeaderCell>model</HeaderCell>
-										<HeaderCell>vin</HeaderCell>
-										<HeaderCell>ein</HeaderCell>
-										<HeaderCell>owner</HeaderCell>
-										<HeaderCell>service</HeaderCell>
-										<HeaderCell>doc URI</HeaderCell>
-								</Row>
-						</Header>
-
-						<Body>
-								<Row>
-										<Cell>122</Cell>
-										<Cell>Test model</Cell>
-										<Cell>Test vin</Cell>
-										<Cell>Test ein</Cell>
-										<Cell>Test owner</Cell>
-										<Cell>Test service</Cell>
-										<Cell>Test Doc URI</Cell>
-								</Row>
-
-								<RequestRow />
-							</Body>
-					</Table>
+					<Body>
+						{this.renderRows()}
+					</Body>
+				</Table>
 
 			</Layout>
 		);
@@ -105,7 +142,7 @@ class ERC721VehicleList extends React.Component {
 // export default ERC721VehicleList;
 export default () => (
 	<Web3Container
-				renderLoading={() => <div>Loading ERC721VehicleList Page...</div>}
+		renderLoading={() => <div>Loading ERC721Vehicle List Page...</div>}
 		render={({ web3, accounts, contract }) => (
 			<ERC721VehicleList accounts={accounts} contract={contract} web3={web3} />
 		)}
