39881403138b5df37501ae7f1900d296a95b800e
Sven Meyer <git_dev@svenmeyer.com>
2018-09-08 14:38:32 +1000

    added vehicles/list

--- client/routes.js
+++ client/routes.js
@@ -1,11 +1,10 @@
 const routes = require('next-routes')();
 
 routes
-  .add('/vehicles/new', '/vehicles/new')
-  .add('/vehicles/list', '/vehicles/index')
-  .add('/vehicles/', '/vehicles/index')
+  .add('/vehicles/new',  '/vehicles/new')
+  .add('/vehicles/list', '/vehicles/list')
   .add('/vehicles/:address', '/vehicles/show')
-  .add('/vehicles/:address/logentries', '/vehicles/logentries/index')
+  .add('/vehicles/:address/logentries',     '/vehicles/logentries/index')
   .add('/vehicles/:address/logentries/new', '/vehicles/logentries/new');
 
 module.exports = routes;
